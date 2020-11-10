dc71d6bb5011dcb47d0fd910562ff3c9f7429b6e
Gerd Reiss <gerd@reiss.pro>
2020-11-10 17:29:10 +0100

    handle exception

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -11,13 +11,16 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            data = response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
 
     title = data["title"]
     extract = data["extract"]
 
     click.secho(title, fg="green")
     click.echo(textwrap.fill(extract))
-
